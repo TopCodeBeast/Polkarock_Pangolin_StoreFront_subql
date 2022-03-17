e7912a4b4e1a469d87093e3b0eb83535adc947be
TopCodeBeast <maxb47163@gmail.com>
2022-03-17 11:48:53 +0900

    add update

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -1,6 +1,7 @@
 import { Call as TCall } from "@polkadot/types/interfaces";
 import { EventRecord } from '@polkadot/types/interfaces';
 import { SubstrateExtrinsic } from "@subql/types";
+// const PREFIXES = ['0x726d726b', '0x524d524b']
 const PREFIXES = ['0x726d726b', '0x524d524b']
 // import { encodeAddress } from "@polkadot/util-crypto";
 
@@ -22,11 +23,16 @@ export interface RemarkResultEntity extends RemarkResult {
   id: string;
 }
 
+// export const isSystemRemark = (call: TCall, prefixes: string[] = PREFIXES): boolean =>
+//   call.section === "system" &&
+//   call.method === "remark" &&
+//   (prefixes.length < 1 ||
+//     prefixes.some((word) => call.args.toString().startsWith(word)));
+
+
 export const isSystemRemark = (call: TCall, prefixes: string[] = PREFIXES): boolean =>
   call.section === "system" &&
-  call.method === "remark" &&
-  (prefixes.length < 1 ||
-    prefixes.some((word) => call.args.toString().startsWith(word)));
+  call.method === "remark"
 
 export const isUtilityBatch = (call: TCall) =>
   call.section === "utility" &&
